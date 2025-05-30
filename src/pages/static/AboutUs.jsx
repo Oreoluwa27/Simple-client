a7937288a71f823ed9c31b6cf850be7bd6eccd53
oreoluwa <akintomowooreoluwa@gmail.com>
2025-05-30 23:33:11 +0100

    test workflow

--- src/pages/static/AboutUs.jsx
+++ src/pages/static/AboutUs.jsx
@@ -4,7 +4,7 @@ import { Col, Row } from "react-bootstrap";
 function AboutUs() {
   return (
     <Layout>
-      <h3 className="text-center">About Us</h3>
+      <h3 className="text-center">About Us!</h3>
       <Row className="justify-content-center">
         <Col md={6}>About Us, Lorem </Col>
       </Row>
